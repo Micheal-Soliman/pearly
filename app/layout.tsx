0c06ab63d3037738070321922c534530d8569d06
Micheal Soliman <michealsoliman2@gmail.com>
2026-04-25 11:19:10 +0300

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,7 +57,7 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1816841682327637');
+fbq('init', '1349687783717801');
 fbq('track', 'PageView');`}
         </Script>
         <Script id="tiktok-pixel" strategy="afterInteractive">
@@ -74,7 +74,7 @@ fbq('track', 'PageView');`}
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1816841682327637&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1349687783717801&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
